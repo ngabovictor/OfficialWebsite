e48d19d2bbd13e4ce209a56ffbc43a8a4f55bde2
ngabovictor <nvichack@gmail.com>
2017-02-18 01:50:15 +0200

    admin static files

--- OfficialWebsite/wsgi.py
+++ OfficialWebsite/wsgi.py
@@ -15,7 +15,6 @@ if path not in sys.path:
 	sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFileHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OfficialWebsite.settings")
 
